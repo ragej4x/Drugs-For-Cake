4c1b85edb6f216c0d8c4d033f4c01c0d3f6d8259
ragej4x <tongaojimboy@gmail.com>
2022-11-27 03:35:45 +0800

    deleted:    data/data 	new file:   data/diag.json 	modified:   main.py

--- main.py
+++ main.py
@@ -820,8 +820,16 @@ class npc_class():
 		self.font = pg.font.Font("data/bin/font" , 20, bold = True)
 		self.speed = 5
 
+		self.narr_diag_1 = False
+
 		self.player_diag_1 = False
-		self.player_diag_2 = True
+		self.player_diag_2 = False
+		self.player_diag_3 = False
+		self.player_diag_4 = False
+		self.player_diag_5 = False
+		self.player_diag_6 = False
+
+		self.strger_diag_1 = True
 
 	def update_npc(self):
 		jubirt_rect = pg.draw.rect(window,(0,0,200), (500 - player.camera_x ,500 - player.camera_y , 30,30))
@@ -831,14 +839,47 @@ class npc_class():
 		font = pg.font.Font("data/bin/font" , 10, bold = True)
 		skip = font.render("PRESS : L_SHIFT TO SKIP.",True , (255,255,255))
 		enter = font.render("PRESS : ENTER TO CONTINUE.",True ,(255,255,255))
+		
+		# NARRATOR DIAG 1 ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+		if self.narr_diag_1 == True:
+			narr_diag_1 = {"diag_1":"Tomorrow is Roshela's birthday and you need to buy a cake for her 16th birthday",
+			"diag_2":"but you dont have any money."}
+			
+
+			narr_diag_txt_1 = self.font.render(narr_diag_1["diag_1"][0:self.txt_animation//self.speed], True ,(255,255,255))
+
+			display.blit(narr_diag_txt_1,(5,405))
+
+			if  self.txt_animation >= len(narr_diag_1["diag_1"]) + 150:
+				narr_diag_txt_1 = self.font.render(narr_diag_1["diag_2"][0:self.txt_animation//self.speed - 120], True ,(255,255,255))
+				display.blit(narr_diag_txt_1,(5,435))
+				
+				if self.txt_animation >= len(narr_diag_1["diag_2"]) + 560:
+					self.txt_animation = 10000
+
+			self.txt_animation += 1
+
+			if self.txt_animation <= 10000:
+				display.blit(skip,(650,500))
+
+			if self.txt_animation >= 10000:
+				display.blit(enter,(650,500))
+
+			if keyinput[pg.K_LSHIFT]:
+				self.txt_animation = 10000
+
+			if keyinput[pg.K_RETURN] and self.txt_animation >= 10000 - 1:
+					self.narr_diag_1 = False
+					self.txt_animation = 0
 
 		# DIAG 1 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 		if self.player_diag_1 == True:
 			disable_movement = True
 
-			player_diag_1 = {"diag_1" :"Tomorrow  is  roshela's  birthday  WTF!!" , 
-			"diag_2":"I  dont  have  any  money  to  buy  her  a  gift."}
+			player_diag_1 = {"diag_1" :"Tomorrow is roshela's birthday WTF!!" , 
+			"diag_2":"I dont have any money to buy her a gift."}
 
 			player_diag_txt_1 = self.font.render(player_diag_1["diag_1"][0:self.txt_animation//self.speed], True ,(255,255,255))
 
@@ -854,18 +895,19 @@ class npc_class():
 
 			self.txt_animation += 1
 
-			if self.txt_animation <= 10000:
-				display.blit(skip,(650,500))
-
-			if self.txt_animation >= 10000:
-				display.blit(enter,(650,500))
 
 			if keyinput[pg.K_LSHIFT]:
 				self.txt_animation = 10000
 
-			if keyinput[pg.K_RETURN] and self.txt_animation >= 10000 - 1:
+			if keyinput[pg.K_RETURN] and self.txt_animation >= 10000:
 					self.player_diag_1 = False
 					self.txt_animation = 0
+
+			if self.txt_animation <= 10000 - 1:
+				display.blit(skip,(650,500))
+
+			if self.txt_animation >= 10000:
+				display.blit(enter,(650,500))
 
 		# DIAG 2 +++++++++++++++++++++++++++++++++++=
 
@@ -890,11 +932,99 @@ class npc_class():
 				display.blit(enter,(650,500))
 
 			#diag_box_1 = pg.draw.rect(display,(30,30,30),(0,400, width,200))
+		
+		# DIAG 3 ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+		if self.player_diag_3 == True:
+			player_diag_3 = {"diag_1":"Huh who tf are you?"}
+			diag_3 = self.font.render(player_diag_3["diag_1"][0:self.txt_animation//self.speed], True, (255,255,255))
+			display.blit(diag_3,(5,405))
+
+			self.txt_animation += 1
+
+			if keyinput[pg.K_LSHIFT]:
+				self.txt_animation = 100
+
+			if keyinput[pg.K_RETURN] and self.txt_animation >= 100 - 1:
+					self.player_diag_3 = False
+					self.txt_animation = 0
+
+			if self.txt_animation <= 100 - 1:
+				display.blit(skip,(650,500))
+
+			if self.txt_animation >= 100:
+				display.blit(enter,(650,500))
+
+		# DIAG 4 ++++++++++++++++++++++++++++++++++++++++++++++++
+
+		if self.player_diag_4 == True:
+			player_diag_4 = {"diag_1":"Ok but how?"}
+			diag_4 = self.font.render(player_diag_4["diag_1"][0:self.txt_animation//self.speed], True, (255,255,255))
+			display.blit(diag_4,(5,405))
+
+			self.txt_animation += 1
+
+			if keyinput[pg.K_LSHIFT]:
+				self.txt_animation = 80
+
+			if keyinput[pg.K_RETURN] and self.txt_animation >= 80 - 1:
+					self.player_diag_4 = False
+					self.txt_animation = 0
+
+			if self.txt_animation <= 80 - 1:
+				display.blit(skip,(650,500))
+
+			if self.txt_animation >= 80:
+				display.blit(enter,(650,500))
+
+		# DIAG 5 +++++++++++++++++++++++++++++++++++++++++++++++++
 
 
+		if self.player_diag_5 == True:
+			player_diag_5 = {"diag_1":"HUH? ARE YOU CRAZY??!!"}
+			diag_5 = self.font.render(player_diag_5["diag_1"][0:self.txt_animation//self.speed], True, (255,255,255))
+			display.blit(diag_5,(5,405))
 
+			self.txt_animation += 1
 
+			if keyinput[pg.K_LSHIFT]:
+				self.txt_animation = 110
 
+			if keyinput[pg.K_RETURN] and self.txt_animation >= 110 - 1:
+					self.player_diag_5 = False
+					self.txt_animation = 0
+
+			if self.txt_animation <= 110 - 1:
+				display.blit(skip,(650,500))
+
+			if self.txt_animation >= 110:
+				display.blit(enter,(650,500))
+
+		# DIAG 6 +++++++++++++++++++++++++++++++++++++++++++++++++
+
+		if self.player_diag_6 == True:
+			player_diag_6 = {"diag_1":"Hmmm.... Ok im in."}
+			diag_6 = self.font.render(player_diag_6["diag_1"][0:self.txt_animation//self.speed], True, (255,255,255))
+			display.blit(diag_6,(5,405))
+
+			self.txt_animation += 1
+
+			if keyinput[pg.K_LSHIFT]:
+				self.txt_animation = 110
+
+			if keyinput[pg.K_RETURN] and self.txt_animation >= 110 - 1:
+					self.player_diag_6 = False
+					self.txt_animation = 0
+
+			if self.txt_animation <= 110 - 1:
+				display.blit(skip,(650,500))
+
+			if self.txt_animation >= 110:
+				display.blit(enter,(650,500))
+
+		# STRANGER DIAG 1 +++++++++++++++++++++++++++++++++++++++++
+			strger_diag_6 = {"diag_1":"Hmmm.... Ok im in."}
+	
 
 npc = npc_class()
 # MAINLOOP +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -915,7 +1045,6 @@ while loop == True:
 	player.player_mech()
 	map.city_border()
 	map.collider()
-
 
 #DISPLAY
 	display_fps()
